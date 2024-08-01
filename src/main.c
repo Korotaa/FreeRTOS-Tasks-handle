e1a148f62b4da5d03d527c81aa225d4b0cce9af5
Korota Arsène COULIBALY <korotaarsenecoulibaly@gmail.com>
2024-08-01 19:44:14 +0100

    Second commit

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
-
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+
+void xTask1(void *parameter);
+void xTask2(void *parameter);
+void xTask3(void *parameter);
 
 
-
-
-svoid app_main() {}
+void app_main() 
+{
+    
+}
